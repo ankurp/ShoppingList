cee7729cd9046b9b77b7da5894c57f32c1796829
Ankur Patel <ankur.patel@ymail.com>
2017-10-25 14:34:28 -0400

    Init

---
+++ ShoppingList/Models/Item.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class Item: Codable {
+  var name: String
+  var isChecked: Bool
+
+  init(name: String, isChecked: Bool = false) {
+      self.name = name
+      self.isChecked = isChecked
+  }
+  
+  static func fake(_ count: Int) -> [Item] {
+    var items = [Item]()
+    for i in 0...count {
+      let item = Item(name: "Item \(i)", isChecked: i % 2 == 0)
+      items.append(item)
+    }
+    return items
+  }
+  
+  func toggleCheck() -> Item {
+    return Item(name: name, isChecked: !isChecked)
+  }
+}
